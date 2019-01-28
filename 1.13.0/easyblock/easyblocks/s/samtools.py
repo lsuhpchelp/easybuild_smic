01eba820c779f0457867d20a079465a6f0402cb3
Le Yan <lyan1@tigers.lsu.edu>
2019-01-28 12:17:29 -0600

    Added a few eb files.

--- 1.13.0/easyblock/easyblocks/s/samtools.py
+++ 1.13.0/easyblock/easyblocks/s/samtools.py
@@ -34,54 +34,75 @@ class EB_samtools(ConfigureMake):
         """Define lists of files to install."""
         super(EB_samtools, self).__init__(*args, **kwargs)
 
-        self.bin_files = ["bcftools/vcfutils.pl", "bcftools/bcftools", "misc/blast2sam.pl",
+	self.bin_files = ["misc/blast2sam.pl",
                           "misc/bowtie2sam.pl", "misc/export2sam.pl", "misc/interpolate_sam.pl",
                           "misc/novo2sam.pl", "misc/psl2sam.pl", "misc/sam2vcf.pl", "misc/samtools.pl",
                           "misc/soap2sam.pl", "misc/varfilter.py", "misc/wgsim_eval.pl",
                           "misc/zoom2sam.pl", "misc/md5sum-lite", "misc/md5fa", "misc/maq2sam-short",
                           "misc/maq2sam-long", "misc/wgsim", "samtools"]
+
+	self.include_files = []
+	
+	self.lib_files = []
+	
+	if LooseVersion(self.version) <= LooseVersion('1.2'):
+            self.include_files += ["bam.h", "bam2bcf.h", "bam_endian.h", "errmod.h",
+                              "kprobaln.h",  "sam.h", "sam_header.h", "sample.h"]
+            self.lib_files += ["libbam.a"]
+	
         if LooseVersion(self.version) <= LooseVersion('0.1.18'):
             # seqtk is no longer there in v0.1.19
             self.bin_files += ["misc/seqtk"]
-        elif LooseVersion(self.version) >= LooseVersion('0.1.19'):
+        elif LooseVersion(self.version) >= LooseVersion('0.1.19') and LooseVersion(self.version) < LooseVersion('1.0'):
             # new tools in v0.1.19
-            self.bin_files += ["misc/ace2sam", "misc/bamcheck","misc/plot-bamcheck","misc/r2plot.lua",
+            self.bin_files += ["misc/ace2sam", "misc/r2plot.lua",
                                "misc/vcfutils.lua"]
 
-        self.lib_files = ["libbam.a"]
-        self.include_files = ["bam.h", "bam2bcf.h", "bam_endian.h", "bgzf.h", "errmod.h", "faidx.h", "kaln.h",
-                              "khash.h", "klist.h", "knetfile.h", "kprobaln.h", "kseq.h", "ksort.h", "kstring.h",
-                              "razf.h", "sam.h", "sam_header.h", "sample.h"]
+        if LooseVersion(self.version) >= LooseVersion('0.1.19') and LooseVersion(self.version) < LooseVersion('1.0'):
+            self.bin_files += ["misc/bamcheck", "misc/plot-bamcheck"]
 
-    def configure_step(self):
-        """
-        No configure
-        """
-        pass
+        if LooseVersion(self.version) < LooseVersion('1.0'):
+            self.bin_files += ["bcftools/vcfutils.pl", "bcftools/bcftools"]
+            self.include_files += [ "bgzf.h", "faidx.h",  "khash.h", "klist.h", "knetfile.h", "razf.h",
+                                    "kseq.h", "ksort.h", "kstring.h"]
+        elif LooseVersion(self.version) >= LooseVersion('1.0'):
+            self.bin_files += ["misc/plot-bamstats","misc/seq_cache_populate.pl"]
 
-    def install_step(self):
-        """
-        Install by copying files to install dir
-        """
+        if LooseVersion(self.version) < LooseVersion('1.2'):
+            # kaln aligner removed in 1.2 (commit 19c9f6)
+            self.include_files += ["kaln.h"]
 
-        for (srcdir, dest, files) in [
-                                      (self.cfg['start_dir'], 'bin', self.bin_files),
-                                      (self.cfg['start_dir'], 'lib', self.lib_files),
-                                      (self.cfg['start_dir'], 'include/bam', self.include_files)
-                                     ]:
 
-            destdir = os.path.join(self.installdir, dest)
-            srcfile = None
-            try:
-                os.makedirs(destdir)
-                for filename in files:
-                    srcfile = os.path.join(srcdir, filename)
-                    shutil.copy2(srcfile, destdir)
-            except OSError, err:
-                self.log.error("Copying %s to installation dir %s failed: %s" % (srcfile, destdir, err))
+
+#    def configure_step(self):
+#        """
+#        No configure
+#        """
+#        pass
+
+#    def install_step(self):
+#        """
+#        Install by copying files to install dir
+#        """
+
+#        for (srcdir, dest, files) in [
+#                                      (self.cfg['start_dir'], 'bin', self.bin_files),
+#                                      (self.cfg['start_dir'], 'lib', self.lib_files),
+#                                      (self.cfg['start_dir'], 'include/bam', self.include_files)
+#                                     ]:
+
+#            destdir = os.path.join(self.installdir, dest)
+#            srcfile = None
+#            try:
+#                os.makedirs(destdir)
+#                for filename in files:
+#                    srcfile = os.path.join(srcdir, filename)
+#                    shutil.copy2(srcfile, destdir)
+#            except OSError, err:
+#                self.log.error("Copying %s to installation dir %s failed: %s" % (srcfile, destdir, err))
 
         # fix permissions so ownwer group and others have R-X
-        adjust_permissions(self.installdir, stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH, add=True, recursive=True)
+#        adjust_permissions(self.installdir, stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH, add=True, recursive=True)
 
     def sanity_check_step(self):
         """Custom sanity check for SAMtools."""
